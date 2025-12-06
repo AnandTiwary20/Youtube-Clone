63ff0bfce37281ade88374a17deb8b11e0704d5f
AnandTiwary20 <anandtiwari0019@gmail.com>
2025-12-06 13:15:58 +0530

    updated auth and store js

--- Frontend/src/store/authSlice.js
+++ Frontend/src/store/authSlice.js
@@ -1,12 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const token = localStorage.getItem("token");
+const user = JSON.parse(localStorage.getItem("user"));
 
 const authSlice = createSlice({
   name: "auth",
   initialState: {
     token: token || null,
-    user: null,
+    user: user || null,
     isAuthenticated: !!token,
   },
   reducers: {
@@ -15,12 +16,14 @@ const authSlice = createSlice({
       state.user = action.payload.user;
       state.isAuthenticated = true;
       localStorage.setItem("token", action.payload.token);
+       localStorage.setItem("user", JSON.stringify(action.payload.user));
     },
     logout: (state) => {
       state.token = null;
       state.user = null;
       state.isAuthenticated = false;
       localStorage.removeItem("token");
+       localStorage.removeItem("user");
     },
   },
 });
