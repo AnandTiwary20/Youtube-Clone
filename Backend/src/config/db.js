517384c7a7e0bfb5d67d50859bb308aaec692f7b
AnandTiwary20 <anandtiwari0019@gmail.com>
2025-12-10 00:21:41 +0530

    Updated DB.js for better error handling

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -9,10 +9,22 @@ export const PORT = process.env.PORT || 5000;
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("Mongo connection failed:", err.message);
     process.exit(1);
   }
+
+  mongoose.connection.on("error", (err) => {
+    console.log("MongoDB Error: ", err);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB disconnected. Attempting reconnect...");
+  });
 };
