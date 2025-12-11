15acb13b38f539c9bb87d79f4ef2952beceaced1
AnandTiwary20 <anandtiwari0019@gmail.com>
2025-12-12 00:33:52 +0530

    updated Authcontroller to increase login properly

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -5,40 +5,43 @@ import { validationResult } from "express-validator";
 
 const JWT_SECRET = process.env.JWT_SECRET || "your_secret_key";
 
-// REGISTER
+// REGISTER The user logic is here
 export const register = async (req, res) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() });
 
   try {
     const { username, email, password } = req.body;
 
     const existing = await User.findOne({ $or: [{ email }, { username }] });
-    if (existing) return res.status(400).json({ message: "User already exists" });
+    if (existing)
+      return res.status(400).json({ message: "User already exists" });
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
+    
     const user = await User.create({
       username,
       email,
-      password: hashedPassword,
+      password,  
     });
 
-    const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: "7d" });
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: "7d",
+    });
 
     res.status(201).json({
       message: "Registration successful",
       token,
-      user: { id: user._id, username: user.username, email: user.email }
+      user: { id: user._id, username: user.username, email: user.email },
     });
-
   } catch (err) {
     console.log("Registration Error:", err);
     res.status(500).json({ message: "Server Error", error: err.message });
   }
 };
 
-// LOGIN
+
+// LOGIN for the existing user is here
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -62,7 +65,7 @@ export const login = async (req, res) => {
   }
 };
 
-// GET USER
+// GET USER to fetch logged in user details
 export const getUser = async (req, res) => {
   try {
     const user = await User.findById(req.user._id).select("-password");
